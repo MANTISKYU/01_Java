4ad667c608fc932a4cb93c765483d27984454a91
MANTISKYU <asdaron44@gmail.com>
2024-05-27 17:04:16 +0900

    객체

---
+++ chap07-object-array-lecture-source/src/main/java/com/ohgiraffers/section02/uses/MemberFinder.java
@@ -0,0 +1,12 @@
+package com.ohgiraffers.section02.uses;
+
+public class MemberFinder {
+
+    public Member[] findAllMembers() {
+
+        return MemberRepository.findAllMembers();
+
+
+    }
+
+}
