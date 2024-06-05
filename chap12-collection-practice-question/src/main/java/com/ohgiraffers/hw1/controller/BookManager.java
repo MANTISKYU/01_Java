f679d24025cb1fd216e3911d371de979c6774a58
MANTISKYU <asdaron44@gmail.com>
2024-06-05 18:11:52 +0900

    콜렉션 문제

---
+++ chap12-collection-practice-question/src/main/java/com/ohgiraffers/hw1/controller/BookManager.java
@@ -0,0 +1,55 @@
+package com.ohgiraffers.hw1.controller;
+
+
+import com.ohgiraffers.hw1.model.dto.BookDTO;
+
+import java.util.ArrayList;
+
+public class BookManager {
+
+    private ArrayList<BookDTO> bookList;
+
+    public void addBook(book) {
+
+
+
+    }
+
+    public void deleteBook(index) {
+
+
+    }
+
+    public int searchBook(bTitle) {
+
+
+    }
+
+
+    public void printBook(index) {
+
+
+
+    }
+
+    public void displayAll() {
+
+
+    }
+
+    public ArrayList<BookDTO> sortedBookList(select) {
+
+
+    }
+
+    public void printBookList(ArrayList<BookDTO>) {
+
+
+
+
+    }
+
+
+
+
+}
